cae9789640e39585cc79f8cea3f978a13f9a610b
FelixRilling <contact@rilling.dev>
2021-11-28 11:48:25 +0100

    Improved CLI output.

--- src/main/java/org/felixrilling/musql/MusqlApplication.java
+++ src/main/java/org/felixrilling/musql/MusqlApplication.java
@@ -2,10 +2,13 @@ package org.felixrilling.musql;
 
 import org.felixrilling.musql.core.FileEntity;
 import org.felixrilling.musql.core.FileEntityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -14,6 +17,8 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class MusqlApplication implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MusqlApplication.class);
 
 	private final FileEntityService fileEntityService;
 
@@ -26,11 +31,18 @@ public class MusqlApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		Set<Path> paths = Arrays.stream(args).map(Paths::get).collect(Collectors.toSet());
+		LOGGER.info("Starting import of {} files.", paths.size());
 		for (Path path : paths) {
-			FileEntity fileEntity = fileEntityService.loadFile(path);
-			fileEntityService.save(fileEntity);
+			try {
+				FileEntity fileEntity = fileEntityService.loadFile(path);
+				LOGGER.info("Read file '{}'.", path);
+				fileEntityService.save(fileEntity);
+				LOGGER.info("Imported file '{}'.", path);
+			} catch (IOException e) {
+				LOGGER.error("Could not read/import '{}'.", path, e);
+			}
 		}
 	}
 }
