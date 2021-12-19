923b634efd13c923703d1429b9063d74f0fc15a1
FelixRilling <contact@rilling.dev>
2021-12-19 13:05:19 +0100

    Improved schema.

--- src/main/resources/db/migration/V1_0__create_musql_schema.sql
+++ src/main/resources/db/migration/V1_0__create_musql_schema.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA musql;
+CREATE SCHEMA IF NOT EXISTS musql;
 
 CREATE TABLE musql.file
 (
@@ -12,20 +12,22 @@ CREATE TABLE musql.file_tag
 	id      BIGINT  NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	file_id BIGINT  NOT NULL,
 	key     VARCHAR NOT NULL,
-	value   TEXT    NOT NULL,
+	value   VARCHAR NOT NULL,  /* Maybe use TEXT instead, but h2 does not support indexing it. */
 	CONSTRAINT file_tag_file_fk FOREIGN KEY (file_id) REFERENCES musql.file (id) ON DELETE CASCADE
 );
 
-CREATE INDEX file_tag_key_ix ON musql.file_tag (key);
+CREATE INDEX file_tag_key_value_ix ON musql.file_tag (key, value);
 
-CREATE VIEW musql.genre_popularity AS
+
+
+CREATE VIEW musql.demo_genre_popularity AS
 SELECT t.value, COUNT(t.value)
 FROM musql.file_tag t
 WHERE t.key = 'genre'
 GROUP BY t.value
 ORDER BY COUNT(t.value) DESC;
 
-CREATE VIEW musql.tracks_from_cd AS
+CREATE VIEW musql.demo_tracks_from_cd AS
 SELECT f.path
 FROM musql.file f
 WHERE f.id IN
