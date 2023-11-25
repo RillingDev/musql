a46cdc058bd5012e175ce61aaa6a0575196a0c18
FelixRilling <contact@rilling.dev>
2023-11-25 12:39:12 +0100

    Remove flyway.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 -- This schema is built to work in both PostgreSQL and H2.
 
-CREATE SCHEMA musql;
+CREATE SCHEMA IF NOT EXISTS musql;
 
-CREATE TABLE musql.file
+CREATE TABLE IF NOT EXISTS musql.file
 (
 	id            BIGINT    NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	path          VARCHAR   NOT NULL UNIQUE,
@@ -11,7 +11,7 @@ CREATE TABLE musql.file
 
 -- Note that even for a single combination of `file_id` and `name`,
 -- multiple rows may exist in the case of multi-valued tags.
-CREATE TABLE musql.file_tag
+CREATE TABLE IF NOT EXISTS musql.file_tag
 (
 	id      BIGINT  NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	file_id BIGINT  NOT NULL,
@@ -23,25 +23,25 @@ CREATE TABLE musql.file_tag
 );
 
 -- Useful for most queries operating on tags as they almost always go by name, and often go by value.
-CREATE INDEX file_tag_name_value_ix ON musql.file_tag (name, val);
+CREATE INDEX IF NOT EXISTS file_tag_name_value_ix ON musql.file_tag (name, val);
 
 
 
-CREATE VIEW musql.demo_genre_popularity AS
+CREATE OR REPLACE VIEW musql.demo_genre_popularity AS
 SELECT t.val, COUNT(t.val)
 FROM musql.file_tag t
 WHERE t.name = 'genre'
 GROUP BY t.val
 ORDER BY COUNT(t.val) DESC;
 
-CREATE VIEW musql.demo_year_popularity AS
+CREATE OR REPLACE VIEW musql.demo_year_popularity AS
 SELECT t.val, COUNT(t.val)
 FROM musql.file_tag t
 WHERE t.name = 'originalyear'
 GROUP BY t.val
 ORDER BY t.val;
 
-CREATE VIEW musql.demo_tracks_from_cd AS
+CREATE OR REPLACE VIEW musql.demo_tracks_from_cd AS
 SELECT f.path
 FROM musql.file f
 		 LEFT JOIN musql.file_tag t ON f.id = t.file_id
@@ -49,7 +49,7 @@ WHERE t.name = 'media'
   AND t.val = 'CD'
 ORDER BY f.path;
 
-CREATE VIEW musql.demo_tracks_missing_genres AS
+CREATE OR REPLACE VIEW musql.demo_tracks_missing_genres AS
 SELECT f.path
 FROM musql.file f
 WHERE f.id NOT IN
