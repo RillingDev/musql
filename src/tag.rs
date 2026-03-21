168ae1d28d7012c6a7a32ad317f8769b9926785e
RillingDev <contact@rilling.dev>
2026-03-21 10:07:02 +0100

    Cleanup.

--- src/tag.rs
+++ src/tag.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use crate::tag_key::CanonicalTagKey;
 use anyhow::Result;
-use log::{debug, info, warn};
+use log::{debug, warn};
 use symphonia::core::formats::FormatOptions;
 use symphonia::core::io::{MediaSourceStream, MediaSourceStreamOptions};
 use symphonia::core::meta::{MetadataOptions, Tag};
@@ -22,22 +22,28 @@ pub fn read_tags(file_path: &Path) -> Result<Tags> {
 
 	let mss = MediaSourceStream::new(Box::new(src), MediaSourceStreamOptions::default());
 
-	let meta_opts: MetadataOptions = MetadataOptions::default();
-	let fmt_opts: FormatOptions = FormatOptions::default();
-	let mut probed = symphonia::default::get_probe().format(&hint, mss, &fmt_opts, &meta_opts)?;
+	let mut probed = symphonia::default::get_probe().format(
+		&hint,
+		mss,
+		&FormatOptions::default(),
+		&MetadataOptions::default(),
+	)?;
 
+	// Based on https://github.com/pdeljanov/Symphonia/blob/6dbe658c2c64228c8b43e4f327264a83d9a1c499/symphonia-play/src/main.rs#L517
 	if let Some(metadata_rev) = probed.format.metadata().current() {
 		debug!("Using container format metadata.");
 
 		if probed.metadata.get().as_ref().is_some() {
-			info!("Tags that are part of the container format are preferentially printed.");
-			info!("Not printing additional tags that were found while probing.");
+			warn!(
+				"Tags that are part of the container format are preferentially used, ignoring other metadata."
+			);
 		}
 		Ok(collect_tags(metadata_rev.tags()))
 	} else if let Some(metadata_rev) = probed.metadata.get().as_ref().and_then(|m| m.current()) {
 		debug!("Using probed metadata.");
 		Ok(collect_tags(metadata_rev.tags()))
 	} else {
+		// Currently metadata that exists in the format (e.g. WAV) is ignored.
 		warn!("No metadata found.");
 		Ok(vec![])
 	}
